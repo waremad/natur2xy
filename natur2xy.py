773d3bdd51e8947c960d4d7b8646b2f4fbd50612
waremad <mycarline5@gmail.com>
2024-12-02 12:45:44 +0900

    0~に修正

--- natur2xy.py
+++ natur2xy.py
@@ -1,21 +1,24 @@
 #
 m = 2
 n = 2
-hw = 15
+hw = 10
 
 def xy2natur(x,y):
+    x,y = x+1,y+1
     out = y + (y+x-2)*(y+x-1)/2
-    out = int(out%10)
+    out -= 1
     return out
 
 def natur2xy(n):
+    n = n+1
     x = 0
     y = 0 
     while not(x*(x+1)/2 >= n):
         x += 1
     while not(x*(x-1)/2 + y == n):
         y += 1
-    x -= y-1
+    x -= y
+    y -= 1
     return (x,y)
 
 
@@ -23,17 +26,13 @@ ls = []
 for i in range(int((hw*1.5)//1)):
     ls.append([" "] * int((hw*1.5)//1))
 for i in range(hw**2):
-    ls[natur2xy(i+1)[1]-1][natur2xy(i+1)[0]-1] = str((i+1)%10)
+    ls[natur2xy(i)[1]][natur2xy(i)[0]] = str((i)%10)
 for i in ls:
     print("".join(i))
 
-    
-"""
-for i in range(hw):
-  for j in range(hw):
-    y = i + 1
-    x = j + 1
+
+for y in range(hw):
+  for x in range(hw):
     out = xy2natur(x,y)
-    print(out,end="")
+    print(int(out%10),end="")
   print()
-"""
--- test_natur2xy.py
+++ test_natur2xy.py
@@ -1,9 +1,28 @@
 from natur2xy import *
 
+def test_xy2natur():
+    assert xy2natur(0,0) == 0
+    assert xy2natur(1,0) == 1
+    assert xy2natur(0,1) == 2
+    assert xy2natur(2,0) == 3
+    assert xy2natur(1,1) == 4
+    assert xy2natur(0,2) == 5
+    assert xy2natur(3,0) == 6  
+    assert xy2natur(2,1) == 7
+    assert xy2natur(1,2) == 8
+    assert xy2natur(0,3) == 9
+    assert xy2natur(4,0) == 10
+    assert xy2natur(3,1) == 11  
+
 def test_natur2xy():
-    assert natur2xy(1) == (1,1)
-    assert natur2xy(2) == (2,1)
-    assert natur2xy(3) == (1,2)
-    assert natur2xy(4) == (3,1)
-    assert natur2xy(5) == (2,2)
-    assert natur2xy(6) == (1,3)
+    assert natur2xy(0) == (0,0)
+    assert natur2xy(1) == (1,0)
+    assert natur2xy(2) == (0,1)
+    assert natur2xy(3) == (2,0)
+    assert natur2xy(4) == (1,1)
+    assert natur2xy(5) == (0,2)
+    assert natur2xy(6) == (3,0)
+
+def test_natur2xy2natur():
+    for i in range(1000):
+        assert i == xy2natur(natur2xy(i)[0],natur2xy(i)[1])
